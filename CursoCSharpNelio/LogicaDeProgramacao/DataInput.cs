fda5dc735fbd798e6229206e1d86bb5e7a951ba2
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-05-24 12:58:38 -0300

    Data output, project refactored

---
+++ CursoCSharpNelio/LogicaDeProgramacao/DataInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LogicaDeProgramacao
+{
+    internal class DataInput
+    {
+        internal void Exercicio()
+        {
+            Console.WriteLine("Entre com seu nome completo");
+            string nomeCompleto = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int qtdQuartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha)");
+            string[] entrada = Console.ReadLine().Split(' ');
+            string ultimoNome = entrada[0];
+            int idade = int.Parse(entrada[1]);
+            double altura = double.Parse(entrada[2]);
+            Console.WriteLine();
+            Console.WriteLine("Nome completo {0}", nomeCompleto);
+            Console.WriteLine($"Número de quartos: {qtdQuartos}");
+            Console.WriteLine($"Preço do produto: R$ {preco.ToString("F2")}");
+            Console.WriteLine("Ultimo nome: " + ultimoNome );
+            Console.WriteLine($"Idade: {idade}");
+            Console.WriteLine($"Altura: {altura.ToString("F2")}");
+        }
+    }
+}
---
+++ CursoCSharpNelio/LogicaDeProgramacao/DataOutput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace LogicaDeProgramacao
+{
+    public class DataOutput
+    {
+        public void Exercicio()
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine("{0}, cujo preço é $ {1}", produto1, preco1);
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2}");
+            Console.WriteLine($"{idade} anos de idade, código {codigo} e genero: {genero}");
+            Console.WriteLine("Medida com 8 casas decimais {0:F8}", medida);
+            Console.WriteLine($"Arredondando (3 casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: {0:F3}", medida.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
