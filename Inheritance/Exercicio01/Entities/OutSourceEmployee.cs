c0c39a988991d6ec0b6b5c5a9c02a25abd7fc924
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-05-30 15:03:38 -0300

    Inheritance ex01 implemented

---
+++ Inheritance/Exercicio01/Entities/OutSourceEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance.Exercicio01.Entities
+{
+    class OutSourceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutSourceEmployee(string name, int hours, double valuePerHour, double aditionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = aditionalCharge;
+        }
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
---
+++ Inheritance/Exercicio01/Ex01.cs
@@ -0,0 +1,48 @@
+﻿using Inheritance.Exercicio01.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance.Exercicio01
+{
+    class Ex01
+    {
+        public static void Exercicio01()
+        {
+            List<Employee> employees = new List<Employee>();
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i<=n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsourced (y/n)? ");
+                char isOutsourced = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                Employee employee;
+                if (isOutsourced == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine());
+                    employee = new OutSourceEmployee(name,hours, valuePerHour, additionalCharge);
+                }else
+                {
+                    employee = new Employee(name, hours, valuePerHour);                    
+                }
+                employees.Add(employee);
+
+            }
+
+            Console.WriteLine("\nPAYMENTS: ");
+            foreach(var emp in employees)
+            {
+                Console.WriteLine(emp);
+            }
+        }
+    }
+}
